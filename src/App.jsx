e896c497e9320c9c5ddddad8eb23129746a02358
JN <jared.bozh@gmail.com>
2023-03-07 03:33:04 +0800

    preloader

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,28 @@
 import React from 'react';
 import './App.css';
-import Table from './components/table/Table.jsx'
-import Backgrnd from './png/background.gif'
-import Popup from './components/popup/Popup';
+import Gratulation from './components/gratulation/Gratulation';
+import Loader from './components/Loader';
 
 function App() {
+
 	const [loading, setLoading] = React.useState(false);
 
 	React.useEffect(() => {
 		setLoading(true);
 		setTimeout(() => {
 			setLoading(false);
-		}, 300);
+		}, 400);
 	}, []);
+
 
 	return (
 		<>
-			{(loading) ?
-				<div className="loader">
-					<div className="loader-inner">
-						<div className="loader-line-wrap">
-							<div className="loader-line"></div>
-						</div>
-						<div className="loader-line-wrap">
-							<div className="loader-line"></div>
-						</div>
-						<div className="loader-line-wrap">
-							<div className="loader-line"></div>
-						</div>
-						<div className="loader-line-wrap">
-							<div className="loader-line"></div>
-						</div>
-						<div className="loader-line-wrap">
-							<div className="loader-line"></div>
-						</div>
-					</div>
-				</div>
-				: <div className="gratulation">
-					<div className="gratulation__wrapper">
-						<div className="gratulation__background">
-							<img src={Backgrnd} className="gratulation__background-img" alt="#" />
-						</div>
-						<div className="gratulation__table">
-							<Table />
-						</div>
-					</div>
-					<Popup text={'b'} />
-					<Popup text={'s'} />
-					<Popup text={'l'} />
-				</div>
+			{(loading)
+				? <Loader />
+				: <Gratulation />
 			}
-
-
-
 		</>
-	);
+	)
 }
 
 export default App;
